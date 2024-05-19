2d48b99432c4deacf433b24bf477b4d959000917
hilwa173 <mhikma385@gmail.com>
2024-05-19 18:39:04 +0300

    updated

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,8 @@
-  function showNews() {
-            const news = document.querySelector('.news')
-            news.style.display = 'flex'
-        }
-    function hideNews() {
-            const news = document.querySelector('.news')
-            news.style.display = 'none'
-        }      
+function showNews() {
+  const news = document.querySelector('.news');
+  news.style.display = 'flex';
+}
+function hideNews() {
+  const news = document.querySelector('.news');
+  news.style.display = 'none';
+}
