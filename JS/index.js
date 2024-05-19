cb99f57e0ee1344f97ad411664187639d534bb88
hilwa173 <mhikma385@gmail.com>
2024-05-19 18:58:02 +0300

    updated mobile-menu

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-function showNews() {
-  const news = document.querySelector('.news');
-  news.style.display = 'flex';
-}
-function hideNews() {
-  const news = document.querySelector('.news');
-  news.style.display = 'none';
-}
